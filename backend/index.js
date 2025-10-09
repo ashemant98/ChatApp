4159dc9c21d4d98d07537511a5aa96ac3bec7e6b
ashemant98 <ashemant98@gmail.com>
2025-10-09 16:06:05 +0530

    DB connection

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,17 @@ const cors = require("cors");
 
 const app = express();
 dotenv.config();
-
+const connectDB = require("../backend/config/db");
 const authRoute = require("../backend/routes/authRoute");
 
 const PORT = process.env.PORT || 3000;
-app.use(cors())
+app.use(cors());
 app.use("/api/auth", authRoute);
 
+//started server
 app.listen(PORT, () => {
+  //server listening to port
   console.log("server running on port" + PORT);
+  //connection to db
+  connectDB();
 });
